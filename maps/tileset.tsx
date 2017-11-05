b49f89e9bb7245854ec38525d4dc773ddb7f3c91
unknown <tomventom@gmail.com>
2017-11-05 17:36:24 +0200

    mapstuff

---
+++ maps/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="32" tileheight="32" tilecount="5" columns="5">
+ <image source="../LoveProjects/squad.love/assets/tileset.png" width="160" height="32"/>
+</tileset>
---
+++ maps/tilesetnogrid.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tilesetnogrid" tilewidth="32" tileheight="32" tilecount="15" columns="5">
+ <image source="../LoveProjects/squad.love/assets/tilesetnogrid.png" width="160" height="96"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="grass"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" value="swamp"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" value="water"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="name" value="sand"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="name" value="empty"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="name" value="tree"/>
+  </properties>
+ </tile>
+</tileset>
